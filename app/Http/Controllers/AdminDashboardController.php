6412cd7c7360c50baadb1dc155dcde60de4c1df3
khet singh <khetsinghyogi@gmail.com>
2026-05-16 00:55:56 +0530

    solve the issue of auto push code

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -8,7 +8,7 @@ class AdminDashboardController extends Controller
 {
     public function index()
     {
-        dd('admin test');
+        // dd('admin test');
         return view('admin.dashboard');
     }
 }
